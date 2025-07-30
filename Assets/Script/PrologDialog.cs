0c4b31131a7b40abdb7e0383b03774e46435d6af
Pramudya76 <pramudyarama76@gmail.com>
2025-07-30 19:42:01 +0700

    Menambahkan Scene untuk narasi cerita saat tidur

---
+++ Assets/Script/PrologDialog.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrologDialog : MonoBehaviour
+{
+    private String[] line = new string[] {"Dimalam yang tenang"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
